9f271ff3b73fb0fcc41e5d33688b2f17aec2a519
Keo chanmarido <Leooscar.psl@gmail.com>
2024-11-23 12:02:42 +0700

    main

--- DOM/DOM-Challenge-Starting-Files/DOM Challenge Starting Files/index.js
+++ DOM/DOM-Challenge-Starting-Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,6 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "MyName"
+document.querySelectorAll(".list a")[0].style.color="Red";
+document.getElementsByTagName("button")[0].style.backgroundColor="Yellow"
+// document.getElementsByTagName("h1")[0].classList.add("huge")
+document.querySelector("h1").innerHTML = "<strong>Good Bye<strong>"
+document.querySelector("h1").textContent = "OK."
